22b0be9d398f40c0f2907e46c032d503e8add8cf
Abhi Love Devkota <67434971+abhilove1@users.noreply.github.com>
2026-05-28 21:37:48 +0200

    Update personal information in library reservation script

--- abhilove engineer library reservation.py
+++ abhilove engineer library reservation.py
@@ -9,9 +9,9 @@ import time
 from datetime import datetime
 
 # ══════════════════════════════════════════════════════════
-YOUR_NAME      = "Bipana Bidari"
-YOUR_EMAIL     = "bbips1111@gmail.com"
-YOUR_MATRICOLA = "589082"
+YOUR_NAME      = "Abhi Love Devkota"
+YOUR_EMAIL     = "abhilovedevkota@gmail.com"
+YOUR_MATRICOLA = "548454"
 FORM_URL       = "https://antonello.unime.it/prenotazione-postazione-biblioteca/?formid=28"
 # ══════════════════════════════════════════════════════════
 
